59a5bdfa73150e007c781f2cd32b070c040c2d3f
iSoumya <isoumyamahata@gmail.com>
2020-07-23 12:39:21 +0530

    Initial Commit
    Signed-off-by: iSoumya <isoumyamahata@gmail.com>

---
+++ src/main/java/com/hcs/AppStarter.java
@@ -0,0 +1,12 @@
+package com.hcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class AppStarter extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStarter.class, args);
+    }
+}
---
+++ src/main/java/com/hcs/api/ChatController.java
@@ -0,0 +1,23 @@
+package com.hcs.api;
+
+import com.hcs.model.Message;
+import com.hcs.model.OutputMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(final Message message) throws Exception {
+
+        final String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+
+}
